1ed5b78185e5ed38195228ef2adf5b053a100d5c
pranjalsidh905 <sidhpranjal905@gmail.com>
2023-03-03 14:47:52 +0530

    React sign up form

--- myapp2/src/App.js
+++ myapp2/src/App.js
@@ -2,47 +2,73 @@ import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react'
 
-function App() {
+function Login() {
 
   const [name,setName]=useState("")
-  const [fname,fsetName]=useState("")
-  const [mname,msetName]=useState("")
-  const [bname,bsetName]=useState("")
+  const [email,setEmail]=useState("")
+  const [phone,setPhone]=useState("")
+  const [password,setPassword]=useState("")
+  const [confirmPassword,setConfirmPassword] =useState("");
+    
+  const onSubmitFrom = (e) => {
+    e.preventDefault();
+    console.log(name,email,phone,password,confirmPassword);
+    const data ={
+      name: name,
+      phone: phone,
+      email: email,
+      password: password,
+      confirmPassword: confirmPassword,
+    };
+    const stringifyData = JSON.stringify(data);
+    localStorage.setItem("data",stringifyData);
+    console.log(">>>>>>>>>>>>>data",data);
+    console.log(">>>>>>>>>>>>>data",JSON.stringify(data));
+  };
   return (
     
    <div class="container">
     <div class="mainbox">
-
+      <div className="sign-up">
+        <h1>Signup</h1>
+      </div>
+      <form onSubmit={onSubmitFrom}>
     
     <div class="div">
-     
+        {/* <label>Full Name</label><br /> */}
       <input type="text" placeholder="Enter the name" onChange={(m) => setName(m.target.value)}/>
     </div>
-    <p>This is a {name}</p> 
+   
      
     <div class="div">
-      
-      <input type="text" placeholder="Enter the name" onChange={(m)=>fsetName(m.target.value)}/>
+      {/* <label>Email</label><br /> */}
+      <input type="Email" placeholder="Enter the Email" onChange={(m)=>setEmail(m.target.value)}/>
     </div>
-    <p>father name is {fname}</p>
+ 
   
     <div class="div">
-      
-      <input type="text" placeholder="Enter the name" onChange={(m)=>msetName(m.target.value)}/>
+      {/* <label >Phone No.</label><br /> */}
+      <input type="number" placeholder="Enter the Ph.no" onChange={(m)=>setPhone(m.target.value)}/>
     </div>
-    <p>father name is {mname}</p>
+    
  
     <div class="div">
-     
-      <input type="text" placeholder="Enter the name" onChange={(m)=>bsetName(m.target.value)}/>
+      {/* <label htmlFor="">Password</label><br /> */}
+      <input type="Password" placeholder="Enter the Password" onChange={(m)=>setPassword(m.target.value)}/>
     </div>
-    <p>father name is {bname}</p>
+    
 
+    <div class="div">
+      <label htmlFor="">Password</label><br />
+      <input type="Password" placeholder="confiram Password" onChange={(m)=>setConfirmPassword(m.target.value)}/>
+    </div>
+    
 
-    <button>sumbit</button>
+    <button type="submit">Sign Up</button>
+    </form>
   </div>
 </div>
   );
 }
 
-export default App;
+export default Login;
