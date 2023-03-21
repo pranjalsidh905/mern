a57dbae55818cf3d71ea9c6d4cfafad2207d8c73
pranjalsidh905 <sidhpranjal905@gmail.com>
2023-03-21 16:09:39 +0530

    html css bootstrap on work

--- Project/js/jav.js
+++ Project/js/jav.js
@@ -19,3 +19,45 @@ $(document).ready(function () {
     }
   });
 });
+
+const slides = document.querySelectorAll('.slide')
+const next = document.querySelector('.next')
+const prev = document.querySelector('.prev')
+let active = 0
+slides[active].classList.add('active')
+
+// next slide function
+const nextSlide = () =>{
+  active++
+  slides.forEach(slide => {
+    slide.classList.remove('active')
+  })
+  if(active === slides.length){
+    active = 0
+  }
+  
+  slides[active].classList.add('active')
+  
+}
+
+// previous slide function
+const prevSlide = () =>{
+  active--
+  slides.forEach(slide => {
+    slide.classList.remove('active')
+  })
+  if(active === 0){
+    active = slides.length - 1
+  }
+  
+  slides[active].classList.add('active')
+  
+}
+
+next.addEventListener('click', nextSlide)
+prev.addEventListener('click', prevSlide)
+
+// auto play
+ setInterval(() =>{
+   nextSlide()
+ }, 5000)
