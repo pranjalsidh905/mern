aa709e6915fdc497484e77adce531e29e396e8a6
pranjalsidh905 <sidhpranjal905@gmail.com>
2023-04-14 15:09:50 +0530

    Netflix-react

--- firstwebsite/src/index.js
+++ firstwebsite/src/index.js
@@ -13,7 +13,7 @@ import Step5 from "./Component/Step5";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Step4 />
+    <Step5 />
   </React.StrictMode>
 );
 
